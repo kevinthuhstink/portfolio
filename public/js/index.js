113915a382e184419af6421c4b7b2c36db688e00
Kevin Cheng <kev72eat@gmail.com>
2024-02-27 21:41:31 -0500

    unravel top/bottom in opening animation

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,35 @@
-function loadAnimation() {
+function loadAnimations() {
 
   function curtainDraw() {
     const curtain = document.getElementById("curtain")
-    setTimeout(() => curtain.style.display = "none", 2000)
+    curtain.style.animationName = "curtain-slide-out"
+    curtain.style.animationTimingFunction = "ease-in-out"
+    curtain.style.animationDuration = "3s"
+    curtain.style.animationIterationCount = 1
+    setTimeout(() => curtain.style.display = "none", 2900)
+  }
+
+  function unravelBottom() {
+    const bottom = document.getElementById("open-bottom")
+    bottom.style.animationName = "unravel-bottom"
+    bottom.style.animationTimingFunction = "ease-in"
+    bottom.style.animationDuration = "1.5s"
+    bottom.style.animationIterationCount = 1
+    setTimeout(() => bottom.style.display = "none", 1400)
+  }
+
+  function unravelTop() {
+    const top = document.getElementById("open-top")
+    top.style.animationName = "unravel-top"
+    top.style.animationTimingFunction = "ease-in"
+    top.style.animationDuration = "1.5s"
+    top.style.animationIterationCount = 1
+    setTimeout(() => top.style.display = "none", 1400)
   }
 
   curtainDraw()
+  setTimeout(unravelTop, 3000)
+  setTimeout(unravelBottom, 3000)
 }
 
-window.onload = loadAnimation 
+window.onload = loadAnimations
